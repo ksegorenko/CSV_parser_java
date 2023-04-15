da8ee945914493a377b261b909bdf4580a915258
ksegorenko <122515534+ksegorenko@users.noreply.github.com>
2023-04-15 16:39:28 +0700

    Update StatisticTable.java

--- CSV_Parser/StatisticTable.java
+++ CSV_Parser/StatisticTable.java
@@ -1,23 +1,35 @@
 import java.io.IOException;
 import java.io.Reader;
-
-public class Parser {
-    Parser(Reader reader) {
-        this.reader = reader;
-    }
-
-    public String nextWord() throws IOException {
-        StringBuilder word = new StringBuilder("");
-        int charIndex = 0;
-        while(charIndex != -1 && word.toString().equals("")){
-            charIndex = reader.read();
-            while(Character.isLetterOrDigit(charIndex)) {
-                word.append((char) charIndex);
-                charIndex = reader.read();
-            }
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Comparator;
+public class StatisticTable {
+    public StatisticTable(Reader reader) throws IOException{
+        wordMap = new HashMap<String, Integer>();
+        Parser parser = new Parser(reader);
+        while(reader.ready()){
+            String word = parser.nextWord();
+            wordMap.merge(word, 1, (prev, next) -> prev + 1);
+            WordFrequency.totalWordsCount++;
         }
-        return word.toString();
+    }
+    public ArrayList<WordFrequency> sortWords() {
+        var sortedWordsTable = new ArrayList<WordFrequency>();
+        for (Map.Entry<String, Integer> pair : wordMap.entrySet()){
+            WordFrequency elem = new WordFrequency(pair.getKey(), pair.getValue());
+            sortedWordsTable.add(elem);
+        }
+        sortedWordsTable.sort(new CompareElements());
+        return sortedWordsTable;
     }
 
-    private final Reader reader;
+    private HashMap<String, Integer> wordMap;
+}
+
+class CompareElements implements Comparator<WordFrequency> {
+    @Override
+    public int compare(WordFrequency elem1, WordFrequency elem2){
+        return elem1.getWordCount() - elem2.getWordCount();
+    }
 }
